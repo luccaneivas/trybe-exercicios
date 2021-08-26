bdf93bcd39201ea7c3dc600f7b4f4f69e7e989af
Lucca Neiva <luccaneiva12@gmail.com>
2021-08-26 00:50:48 -0300

    Adicionados exercicios de 5 a 9

--- fundamentos/bloco-4-introducao-a-js-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-js-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,5 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+let sum;
+let maior;
+let contaImpar;
+let contaPar;
+let menor;
 
 //EXERCICIO 1
 // for (let number of numbers) {
@@ -7,6 +11,7 @@ let sum = 0;
 // }
 
 //EXERCICIO 2
+// sum = 0;
 // for (let i = 0; i < numbers.length; i += 1){
 //   sum += numbers[i];
 //   console.log(sum);
@@ -14,17 +19,66 @@ let sum = 0;
 
 
 //EXERCICIO 3
+// sum = 0;
 // for (let i = 0; i < numbers.length; i += 1) {
 //   sum += numbers[i];
 // }
 // console.log(sum/numbers.length);
 
 //EXERCICIO 4
-for (let i = 0; i < numbers.length; i += 1) {
-  sum += numbers[i];
+// sum = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   sum += numbers[i];
+// }
+// if (sum/numbers.length > 20){
+//   console.log('Valor maior que 20.');
+// }else {
+//   console.log('Valor menor ou igual a 20.');
+// }
+
+//EXERCICIO 5
+// maior = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (i == 0){
+//     maior = numbers[i];
+//   } 
+//   if (numbers[i] > maior){
+//     maior = numbers[i];
+//   }
+// }
+// console.log(maior);
+
+//EXERCICIO 6
+// contaImpar = 0;
+// contaPar = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i]%2 == 0){
+//     contaPar += 1;
+//   } else {
+//     contaImpar += 1;
+//   }
+// }
+// console.log(contaPar + ' ' + contaImpar);
+
+//EXERCICIO 7
+// menor = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (i == 0){
+//     menor = numbers[i];
+//   } else if (numbers[i] < menor) {
+//     menor = numbers[i];
+//   }
+// }
+// console.log(menor);
+
+//EXERCICIO 8
+let array = []
+for (let i = 1; i <= 25; i += 1) {
+  array.push(i);
 }
-if (sum/numbers.length > 20){
-  console.log('Valor maior que 20.');
-}else {
-  console.log('Valor menor ou igual a 20.');
+//console.log(array);
+
+//EXERCICIO 9
+for (const num of array) {
+  console.log(num/2);
 }
