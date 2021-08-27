5d1b75ff32934392c9e961f2ebb2e91cafcb4153
Lucca Neiva <luccaneiva12@gmail.com>
2021-08-27 01:42:59 -0300

    Adicionados exercicios da aula

---
+++ fundamentos/bloco-4-introducao-a-js-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,51 @@
+let n = 5;
+
+//EXERCICIO 1
+// if (n > 1){
+//   for (let i = 0; i < n; i += 1){
+//     let string = '';
+//     for (let u = 0; u < n; u += 1){
+//       string += '*';
+//     }
+//     console.log(string);
+//   }
+// }
+
+//EXERCICIO 2
+// if (n > 1){
+//   for (let i = n; i > 0; i -= 1){
+//     let string = '';
+//     for (let u = n; u >= i; u -= 1){
+//       string += '*';
+//     }
+//     console.log(string);
+//   }
+// }
+
+//EXERCICIO 3
+// if (n > 1){
+//   for (let i = n; i > 0; i -= 1){
+//     let string = '';
+//     for (let c = i - 1; c > 0; c -= 1){
+//       string += ' ';
+//     }
+//     for (let u = n; u >= i; u -= 1){
+//       string += '*';
+//     }
+//     console.log(string);
+//   }
+// }
+
+//EXERCICIO 4
+// if (n > 1){
+//   for (let i = n; i > 0; i -= 2){
+//     let string = '';
+//     for (let c = i - 1; c > 0; c -= 2){
+//       string += ' ';
+//     }
+//     for (let u = n; u >= i; u -= 1){
+//       string += '*';
+//     }
+//     console.log(string);
+//   }
+// }
