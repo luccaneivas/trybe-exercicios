85105a2c1a217a48aa01bf13897d9aadfdff4725
Lucca Neiva <luccaneiva12@gmail.com>
2021-09-06 19:06:46 -0300

    Terminada parte 1 dos exercicios 4.4 e iniciada parte 2

---
+++ fundamentos/bloco-4-introducao-a-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -0,0 +1,39 @@
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald'
+// };
+
+//EXERCICIO 1
+//console.log("Bem-vinda,", info.personagem);
+
+//EXERCICIO 2
+// info.recorrente = "Sim";
+// console.log(info);
+
+//EXERCICIO 3
+// for (let key in info) {
+//   console.log(key);
+// }
+
+//EXERCICIO 4
+// for (let key in info){
+//   console.log(info[key]);
+// }
+
+//EXERCICIO 5
+// let info2 = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: "O último MacPatinhas",
+//   recorrente: "Sim"
+// };
+
+// console.log(info.personagem + " e " + info2.personagem);
+// console.log(info.origem + " e " + info2.origem);
+// console.log(info.nota + " e " + info2.nota);
+// if (info.recorrente === 'Sim' && info.recorrente === 'Sim'){
+//   console.log('Ambos recorrentes');
+// }
+
+//PARTE 2 FUNÇÕES - 4.4
