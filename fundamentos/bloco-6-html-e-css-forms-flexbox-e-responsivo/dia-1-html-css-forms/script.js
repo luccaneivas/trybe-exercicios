3f0334b0b0ec7126a9a00cf4646a1927eb5941e7
Lucca Neiva <luccaneiva12@gmail.com>
2021-09-17 04:33:00 -0300

    Terminados exs 6.1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -72,7 +72,43 @@ function geraEstados(estados, siglas) {
 geraEstados(estados, siglas);
 
 function validationSubmit(event) {
-  //event.preventDefault();
-
+  event.preventDefault();
+  const dataInicio = document.getElementById('data_inicio');
+  if (dataInicio.value.length > 0 && dataInicio.value.length <= 10) {
+    let validation = dataInicio.value.replace('-', '');
+    let validation2 = validation.replace('-', '');
+    let day = '';
+    let month = '';
+    let year = '';
+    for(let i = 0; i < validation2.length; i += 1) {
+      if (i < 4) {
+        year += validation2[i];
+      } else if (i < 6) {
+        month += validation2[i];
+      } else if (i < 8) {
+        day += validation2[i];
+      }
+    }
+    
+    if (parseInt(day, 10) > 0 && parseInt(day, 10) <= 31 && parseInt(month, 10) > 0 && parseInt(month, 10) <= 12 && parseInt(year, 10) > 0) {
+      criaDiv(day, month, year);
+    } else {
+      alert('DATA INVÁLIDA!!!!!')
+    }
+  } else {
+    alert('DATA INVÁLIDA!!!!!');
+  }
 }
 submit.addEventListener('click', validationSubmit);
+
+function criaDiv(dia, mes, ano) {
+  const div = document.createElement('div');
+  div.innerHTML = `<p>${dia + '-' + '-' + mes + '-' + ano}</p>`
+  document.body.appendChild(div);
+}
+
+// Verificar o formato da data dd/mm/aaaa .
+// O dia deve ser > 0 e <= 31.
+// O mês deve ser > 0 e <= 12.
+// O ano não pode ser negativo.
+// Caso alguma das condições seja inválida no momento do envio do formulário, exibir via alert uma mensagem de erro contextualizada.
